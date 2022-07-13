94014bb42aba84d383c132058ac57679cbe13670
Nandokasiasi02 <nandokasiasi02@students.amikom.ac.id>
2022-07-13 12:46:44 +0700

    first commit

---
+++ Penjualan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPenjualan
+{
+    public class Penjualan
+    {
+
+    public Penjualan(string nota, string, tgl, string customer, string jenis, int total)
+    }
+       this.Nota = nota;
+       this.Tanggal = thl;
+       this.Customer = customer;
+       this.Jenis = jenis;
+       this.TotalNota = total;
+     }
+
+
+
+}
